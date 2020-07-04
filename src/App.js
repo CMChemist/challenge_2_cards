1c28ca261c4e47ef365ba14bee3b5f6ef9457e82
Joseph Trettevik <josephtrettevik@gmail.com>
2020-07-04 10:05:21 -0700

    create card component template

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { boilerPlateData } from "./data.js";
+import Card from "./components/Card"
 
 export default () => {
   const generateCardsFromData = collection => {
@@ -7,6 +8,7 @@ export default () => {
       return (
         <Card 
           id={item.id} 
+          name={item.name}
           price={item.price} 
           descriptions={item.description} 
         />
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,13 @@
 import React from "react";
 
-export default () => {
-    
+const Card = (props) => {
     return (
         <div key={props.id}>
         <h2>{props.name}</h2>
         <h4>{props.price}</h4>
-        <p>{props.description}</p>
+        <p>{props.descriptions}</p>
         </div>
     );
 };
 
-
-
+export default Card;
